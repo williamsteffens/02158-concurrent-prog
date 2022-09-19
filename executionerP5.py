8c14aeac6dff0bfa56f1de7cd83f7b973b310624
williamsteffens <william.f.f.steffens@gmail.com>
2022-09-19 16:20:14 +0200

    sidste HPC

--- executionerP5.py
+++ executionerP5.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # P3 igen
-# with open('./ass1data/p5/programOutputP5T1-20-100500.txt', 'a') as f:
+# with open('./ass1data/p5/programOutputP5T1-20-100-500.txt', 'a') as f:
 #     for i in range(1, 21): 
 #         subprocess.call(['java', '-cp', './src/', 'ass1/Search', 
 #                 '-R', 
@@ -9,21 +9,35 @@ import subprocess
 #                 '-W',
 #                 '4',
 #                 '-d',
-#                 './ass1data/p5/dataP5T1-20-100500.csv',
+#                 './ass1data/p5/dataP5T1-20-100-500.csv',
 #                 'shakespeare.txt', 
 #                 'To be, or not to be, that is the question',
 #                 '{0}'.format(i)], 
 #                 stdout=f, 
 #                 universal_newlines=True)
 
-#     for i in range(25, 101, 5): 
+#     for i in range(30, 101, 10): 
 #         subprocess.call(['java', '-cp', './src/', 'ass1/Search', 
 #                 '-R', 
 #                 '10', 
 #                 '-W',
 #                 '4',
 #                 '-d',
-#                 './ass1data/p5/dataP5T1-20-100500.csv',
+#                 './ass1data/p5/dataP5T1-20-100-500.csv',
+#                 'shakespeare.txt', 
+#                 'To be, or not to be, that is the question',
+#                 '{0}'.format(i)], 
+#                 stdout=f, 
+#                 universal_newlines=True)
+
+#     for i in range(150, 501, 50): 
+#         subprocess.call(['java', '-cp', './src/', 'ass1/Search', 
+#                 '-R', 
+#                 '10', 
+#                 '-W',
+#                 '4',
+#                 '-d',
+#                 './ass1data/p5/dataP5T1-20-100-500.csv',
 #                 'shakespeare.txt', 
 #                 'To be, or not to be, that is the question',
 #                 '{0}'.format(i)], 
@@ -33,7 +47,7 @@ import subprocess
 
 
 # P4 igen
-with open('./ass1data/p5/programOutputP5TH1-20T1-20-100.txt', 'w') as f:
+with open('./ass1data/p5/programOutputP5TH1-20T1-20-100-500.txt', 'a') as f:
     for i in range(1, 21): 
         for j in range(1, 21): 
             subprocess.call(['java', '-cp', './src/', 'ass1.Search', 
@@ -42,7 +56,7 @@ with open('./ass1data/p5/programOutputP5TH1-20T1-20-100.txt', 'w') as f:
                         '-W',
                         '4',
                         '-d',
-                        './ass1data/p5/dataP5TH1-20T1-20-100500.csv',
+                        './ass1data/p5/dataP5TH1-20T1-20-100-500.csv',
                         'shakespeare.txt', 
                         'To be, or not to be, that is the question',
                         '{0}'.format(j),
@@ -50,14 +64,14 @@ with open('./ass1data/p5/programOutputP5TH1-20T1-20-100.txt', 'w') as f:
                         stdout=f,
                         universal_newlines=True)
 
-        for j in range(25, 101, 5): 
+        for j in range(30, 101, 10): 
             subprocess.call(['java', '-cp', './src/', 'ass1.Search', 
                         '-R', 
                         '10', 
                         '-W',
                         '4',
                         '-d',
-                        './ass1data/p5/dataP5TH1-20T1-20-100500.csv',
+                        './ass1data/p5/dataP5TH1-20T1-20-100-500.csv',
                         'shakespeare.txt', 
                         'To be, or not to be, that is the question',
                         '{0}'.format(j),
@@ -72,7 +86,7 @@ with open('./ass1data/p5/programOutputP5TH1-20T1-20-100.txt', 'w') as f:
                         '-W',
                         '4',
                         '-d',
-                        './ass1data/p5/dataP5TH1-20T1-20-100500.csv',
+                        './ass1data/p5/dataP5TH1-20T1-20-100-500.csv',
                         'shakespeare.txt', 
                         'To be, or not to be, that is the question',
                         '{0}'.format(j),
